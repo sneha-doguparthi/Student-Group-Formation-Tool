6e778e19da47d3555f631f1c6c32f964f93b5b4a
Sneha <sneha.seraph@gmail.com>
2020-05-28 11:38:13 -0300

    added signup UI

--- src/main/java/com/asdc/group6/Group6Application.java
+++ src/main/java/com/asdc/group6/Group6Application.java
@@ -2,8 +2,9 @@ package com.asdc.group6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class Group6Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/asdc/group6/controller/SignUpController.java
+++ src/main/java/com/asdc/group6/controller/SignUpController.java
@@ -1,5 +1,13 @@
 package com.asdc.group6.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
 public class SignUpController {
-
+	
+	@GetMapping("/signup")
+	public String userSignup() {
+		return "signup";
+	}
+	
 }
