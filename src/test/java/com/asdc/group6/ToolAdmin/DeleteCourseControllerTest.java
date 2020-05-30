074818f7420f26c0c5ae05ad8544300e9087f97c
PARSAD UPENDRA <uanthwal@gmail.com>
2020-05-29 21:53:59 -0300

    Changes to execute the test

--- src/test/java/com/asdc/group6/ToolAdmin/DeleteCourseControllerTest.java
+++ src/test/java/com/asdc/group6/ToolAdmin/DeleteCourseControllerTest.java
@@ -7,15 +7,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.asdc.group6.Group6Application;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = { Group6Application.class,
 		AdminController.class })
 @AutoConfigureMockMvc
@@ -25,7 +28,7 @@ public class DeleteCourseControllerTest {
 	private MockMvc mockMvc;
 
 	@Test
-	public void testAdminControllerView() throws Exception {
+	public void testDeleteCourseControllerView() throws Exception {
 		this.mockMvc.perform(get("/deleteCourseView")).andDo(print()).andExpect(status().isOk())
 				.andExpect(content().string(containsString("Delete Course Panel")));
 	}
