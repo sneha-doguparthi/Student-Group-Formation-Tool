7f6115ceb1089adc78ff564973063f41576ed002
PARSAD UPENDRA <uanthwal@gmail.com>
2020-06-02 22:34:33 -0300

    Added DB Config details

---
+++ src/main/java/com/asdc/group6/Utilities/ApplicationConstants.java
@@ -0,0 +1,17 @@
+package com.asdc.group6.Utilities;
+
+public class ApplicationConstants {
+
+	// DB Constants
+	public static final String DB_DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+
+	// Add Course Error Codes
+	public static final String COURSE_ADDED = "Course added successfully!";
+	public static final String COURSE_ALREADY_EXISTS = "Course already exists!";
+	public static final String COURSE_ADD_FAILED = "Failed to add course.";
+
+	public static final String COURSE_ALREADY_ADDED_FOR_USER = "Course already added for User!";
+	public static final String COURSE_ROLE_SUCCESS = "Course assigned successfully!";
+	public static final String COURSE_ROLE_FAILED = "Failed to assign course!";
+
+}
