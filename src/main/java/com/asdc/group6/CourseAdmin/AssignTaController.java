1da3aa80404199db651391aece679278cfea6eb9
Jay <jy665593@dal.ca>
2020-06-04 20:14:23 -0300

    Fix passing parameters

--- src/main/java/com/asdc/group6/CourseAdmin/AssignTaController.java
+++ src/main/java/com/asdc/group6/CourseAdmin/AssignTaController.java
@@ -3,7 +3,6 @@ package com.asdc.group6.CourseAdmin;
 import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.asdc.group6.CourseAdmin.Service.AssignTaService;
@@ -18,19 +17,24 @@ public class AssignTaController {
 		this.assignTaService = assignTaService;
 	}
 
-	@GetMapping("/assign-ta")
-	public String searchTa(Model model) {
+	@PostMapping("/assign-ta")
+	public String searchTa(@RequestParam String id, @RequestParam String code, 
+			@RequestParam String name, Model model) {
 		
 		ArrayList<User> users = assignTaService.getAllUsers();
 		model.addAttribute("users", users);
 		
+		model.addAttribute("id", id);
+		model.addAttribute("code", code);
+		model.addAttribute("name", name);
+		
 		return "assign-ta";
 	}
 	
-	@PostMapping("/assign-ta")
-	public String assignTa(@RequestParam String userId) {
+	@PostMapping("/do-assign-ta")
+	public String assignTa(@RequestParam String courseId, @RequestParam String userId) {
 		
-		assignTaService.assignTa(userId, 1);
+		assignTaService.assignTa(userId, Integer.valueOf(courseId));
 		
 		return "assign-ta-success";
 	}
--- src/main/java/com/asdc/group6/CourseAdmin/CourseAdminHomeController.java
+++ src/main/java/com/asdc/group6/CourseAdmin/CourseAdminHomeController.java
@@ -1,18 +1,20 @@
 package com.asdc.group6.CourseAdmin;
 
-import java.util.ArrayList;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import com.asdc.group6.Models.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CourseAdminHomeController {
 
-	@GetMapping("/course-admin-home")
-	public String searchTa(Model model) {
+	@PostMapping("/course-admin-home")
+	public String searchTa(@RequestParam String id, @RequestParam String code, 
+			@RequestParam String name ,Model model) {
+		
+		model.addAttribute("id", id);
+		model.addAttribute("code", code);
+		model.addAttribute("name", name);
 		
 		return "course-admin-home";
 	}
