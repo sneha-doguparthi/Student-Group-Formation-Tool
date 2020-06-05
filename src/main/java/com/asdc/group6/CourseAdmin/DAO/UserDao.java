020a66296f26e0ba8f1a862c27bb91135260797e
YashShah-17 <34470036+yashshah-17@users.noreply.github.com>
2020-06-04 22:26:49 -0300

    DAO file changes

--- src/main/java/com/asdc/group6/CourseAdmin/DAO/UserDao.java
+++ src/main/java/com/asdc/group6/CourseAdmin/DAO/UserDao.java
@@ -3,25 +3,15 @@ package com.asdc.group6.CourseAdmin.DAO;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
-import com.asdc.group6.Models.Student;
 import com.asdc.group6.Models.User;
+import com.asdc.group6.Models.Student;
 
-@Service
-public interface UserDao {
-
-	ArrayList<User> getByEmail(String email);
-
-	Boolean update(User user);
+public interface UserDAO {
 
 	public ArrayList<User> getUserByUserID(ArrayList<Integer> userIds);
-
 	public ArrayList<User> getAll();
-
 	public ArrayList<Integer> getUserID(List<Student> students);
-
 	public boolean addUser(List<Student> newToPortalList);
-
 	public ArrayList<String> getPassword(List<Student> students);
+	
 }
